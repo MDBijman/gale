bcc031546378f4cba947e107e7250f50ffb71e40
Matthijs Bijman <matthijs@bijman.org>
2018-05-31 15:50:49 +0200

    Large lexer speedup by creating own instead of using regex

--- include/utils/lexing/lexer.h
+++ include/utils/lexing/lexer.h
@@ -37,53 +37,152 @@ namespace utils
 		{
 			token_id match(lexer_range& range) const
 			{
-				std::smatch match;
-				if (std::regex_search(range.first, range.second, match, regex_object, std::regex_constants::match_continuous))
+				for (int i = 0; i < tokens.size(); i++)
 				{
-					range = match.suffix();
-					for (unsigned int i = 1; i < match.size(); i++)
-						if (match[i].matched) return regex_group_to_token_id.at(i - 1);
+					auto& token = tokens.at(i);
+
+					auto token_it = token.begin();
+					auto text_it = range.first;
+					bool match = true;
+
+					while(match && !(text_it == range.second) && !(token_it == token.end()))
+					{
+						if (*token_it != *text_it)
+						{
+							match = false;
+							continue;
+						}
+
+						token_it++;
+						text_it++;
+					}
+
+					if (token_it == token.end())
+					{
+						range.first = text_it;
+						return token_ids.at(i);
+					}
 				}
+
+				// \".*?\"
+				if (match_strings)
+				{
+					if (*range.first != '\"')
+						goto match_numbers;
+						
+					for (auto i = range.first + 1; i != range.second; i++)
+					{
+						if (*i == '\"')
+						{
+							range.first = i + 1;
+							return string_token;
+						}
+					}
+
+					goto match_numbers;
+				}
+
+			match_numbers:
+				// \\-?[1-9][0-9]*|0
+				if (match_numbers)
+				{
+					auto i = range.first;
+					if (*i == '0')
+					{
+						range.first = range.first + 1;
+						return number_token;
+					}
+
+					if (*i == '-') i++;
+					if (!isdigit(*i) || (*i == '0')) goto match_keywords;
+					while (i != range.second && isdigit(*i)) i++;
+
+					range.first = i;
+					return number_token;
+				}
+
+			match_keywords:
+				// [a-zA-Z_][a-zA-Z0-9_.]*
+				if (match_keywords)
+				{
+					if (!isalpha(*range.first) && (*range.first != '_')) goto match_fail;
+					auto i = range.first + 1;
+					while (i != range.second)
+					{
+						if (!isalnum(*i) && !(*i == '_') && !(*i == '.'))
+							break;
+						else
+							i++;
+					}
+
+					// If matched at least 1 char
+					if (i != range.first)
+					{
+						range.first = i;
+						return keyword_token;
+					}
+				}
+
+			match_fail:
 				return -1;
 			}
 
-			token_id create_token(std::string regex_rule)
+			token_id create_token(std::string token_to_match)
 			{
-				token_definitions.insert({ token_generator, regex_rule });
+				tokens.push_back(std::move(token_to_match));
+				token_ids.push_back(token_generator);
 				return token_generator++;
 			}
 
-			void compile()
+			token_id create_keyword_token()
 			{
-				regex_group_to_token_id.clear();
+				assert(!match_keywords);
+				match_keywords = true;
+				keyword_token = token_generator;
+				return token_generator++;
+			}
 
-				std::string regex_string;
-				int subgroup = 0;
-				for (auto& rule : token_definitions)
-				{
-					regex_group_to_token_id.insert({ subgroup++, rule.first });
-					regex_string.append("(").append(rule.second).append(")").append("|");
-				}
-				regex_string.pop_back();
-				regex_object = std::regex{ regex_string.c_str() };
+			token_id create_number_token()
+			{
+				assert(!match_numbers);
+				match_numbers = true;
+				number_token = token_generator;
+				return token_generator++;
+			}
+
+			token_id create_string_token()
+			{
+				assert(!match_strings);
+				match_strings = true;
+				string_token = token_generator;
+				return token_generator++;
 			}
 
 			rules& operator=(const rules& other)
 			{
-				token_definitions = other.token_definitions;
+				tokens = other.tokens;
+				token_ids = other.token_ids;
+
 				token_generator = other.token_generator;
 
-				compile();
+				string_token = other.string_token;
+				number_token = other.number_token;
+				keyword_token = other.keyword_token;
+				match_strings = other.match_strings;
+				match_numbers = other.match_numbers;
+				match_keywords = other.match_keywords;
 				return *this;
 			}
 
 		private:
 			token_id token_generator = 1;
 
-			std::unordered_map<size_t, token_id> regex_group_to_token_id;
-			std::unordered_map<token_id, std::string> token_definitions;
+			// special tokens
+			token_id string_token, number_token, keyword_token;
+			bool match_strings = false, match_numbers = false, match_keywords = false;
 
-			std::regex regex_object;
+			std::vector<std::string> tokens;
+			std::vector<token_id> token_ids;
 		};
 
 		enum class error_code
