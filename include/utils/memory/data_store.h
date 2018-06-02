8fe6d25c25b018cb60a280c0dd98d8a9703347fa
Matthijs Bijman <matthijs@bijman.org>
2018-06-02 18:50:28 +0200

    More speedups in dynamic_store and tree traversals

--- include/utils/memory/data_store.h
+++ include/utils/memory/data_store.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <array>
+#include <set>
 
 namespace memory
 {
@@ -49,10 +50,7 @@ namespace memory
 	class dynamic_store
 	{
 		std::vector<T> data;
-		std::vector<bool> occupieds;
-		// @performance this improves speed dramatically when the store is full by avoiding scanning the entire occupieds
-		// maybe additional speed can be gained by bookkeeping all free slots, then this speedup is also gained when
-		// the store is almost full except for some of the last slots
+		std::vector<uint8_t> occupieds;
 		bool is_full = true;
 	public:
 		using index = size_t;
@@ -61,37 +59,44 @@ namespace memory
 		{
 			if (is_full)
 			{
-				occupieds.push_back(true);
 				data.push_back(T());
-				return occupieds.size() - 1;
+				occupieds.push_back(1);
+				return data.size() - 1;
 			}
-
-			auto free_pos = std::find(occupieds.begin(), occupieds.end(), false);
-			assert(free_pos != occupieds.end());
-			*free_pos = true;
-			return std::distance(occupieds.begin(), free_pos);
+			else
+			{
+				auto it = std::find(occupieds.begin(), occupieds.end(), 0);
+				*it = 1;
+				return std::distance(occupieds.begin(), it);
+			}
 		}
 
 		T& get_at(index i)
 		{
-			assert(i < occupieds.size());
-			assert(occupieds[i]);
-			assert(data.size() == occupieds.size());
-			return data.at(i);
+#ifdef _DEBUG
+			assert(occupieds.size() == data.size());
+			assert(i < data.size());
+			assert(occupieds[i] == 1);
+#endif
+			return data[i];
 		}
 
 		bool is_occupied(index i)
 		{
-			assert(i < occupieds.size());
-			assert(data.size() == occupieds.size());
-			return occupieds[i];
+#ifdef _DEBUG
+			assert(occupieds.size() == data.size());
+			assert(i < data.size());
+#endif
+			return occupieds[i] == 1 ? true : false;
 		}
 
 		void free_at(index i)
 		{
-			assert(i < occupieds.size());
-			assert(data.size() == occupieds.size());
-			occupieds[i] = false;
+#ifdef _DEBUG
+			assert(occupieds.size() == data.size());
+			assert(i < data.size());
+#endif
+			occupieds[i] = 0;
 			is_full = false;
 		}
 
