524cf12fe57a5396cd6d9c3de55e0ff9a19d8fa7
Matthijs Bijman <matthijs@bijman.org>
2018-06-06 14:32:25 +0200

    Make parser stage flexible for multiple strategies

--- Compiler/include/fe/pipeline/parser_stage.h
+++ Compiler/include/fe/pipeline/parser_stage.h
@@ -6,18 +6,34 @@
 
 namespace fe
 {
-	class parsing_stage 
+	class parsing_strategy
 	{
 	public:
-		parsing_stage();
+		virtual ~parsing_strategy() {}
+		virtual std::variant<utils::bnf::tree, utils::ebnfe::error> parse(const std::vector<utils::bnf::terminal_node>& in) = 0;
+	};
 
-		std::variant<utils::bnf::tree, utils::ebnfe::error> parse(const std::vector<utils::bnf::terminal_node>& in) 
-		{
-			parser.generate(fe::non_terminals::file);
-			return parser.parse(in);
-		}
-
-	private:
+	class lr_strategy : public parsing_strategy
+	{
 		utils::ebnfe::parser parser;
+	public:
+		lr_strategy();
+		virtual std::variant<utils::bnf::tree, utils::ebnfe::error> parse(const std::vector<utils::bnf::terminal_node>& in) override;
+	};
+
+	class recursive_descent_strategy : public parsing_strategy
+	{
+	public:
+		virtual std::variant<utils::bnf::tree, utils::ebnfe::error> parse(const std::vector<utils::bnf::terminal_node>& in) override;
+	};
+	
+	class parsing_stage 
+	{
+		std::unique_ptr<parsing_strategy> strategy;
+
+	public:
+		parsing_stage(std::unique_ptr<parsing_strategy> imp);
+
+		std::variant<utils::bnf::tree, utils::ebnfe::error> parse(const std::vector<utils::bnf::terminal_node>& in);
 	};
 }
