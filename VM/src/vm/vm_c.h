cafa41405bcdfec62d99c1a9c6fb4c2da7b87faf
Matthijs Bijman <matthijs@bijman.org>
2019-09-12 14:22:22 +0200

    Fix some imports

--- VM/src/vm/vm_c.h
+++ VM/src/vm/vm_c.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdint.h>
+#include "fe/data/bytecode.h"
 
 extern "C" uint16_t *vm_init(void *[]);
-extern "C" uint64_t *vm_interpret(const byte *first);
+extern "C" uint64_t *vm_interpret(const fe::vm::byte *first);
